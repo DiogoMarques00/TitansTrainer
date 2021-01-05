98123cad0cfb68dc68e6e0514a5a8da237268082
Diogo Marques <180323009@estudantes.ips.pt>
2021-01-05 14:06:22 +0000

    Add project files.

---
+++ TitansTrainer/Powerup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TitansTrainer
+{
+   public class Powerup
+    {
+        private int m_Nome;
+        private double m_Bonus;
+
+        public int Nome
+        {
+            get
+            {
+                return m_Nome;
+            }
+            set
+            {
+                m_Nome = value;
+            }
+        }
+
+        public double Bonus
+        {
+            get
+            {
+                return m_Bonus;
+            }
+            set
+            {
+                m_Bonus = value;
+            }
+        }
+            
+
+    }
+}
